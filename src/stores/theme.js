983b37ef070d22ff63128eae3d2e5bb398cded6d
Miranda Neerhof <miranda.neerhof@gmail.com>
2024-09-03 14:43:24 -0300

    create theme store

---
+++ src/stores/theme.js
@@ -0,0 +1,18 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { themeOptions } from "../constants/themeOptions.js";
+
+export const useThemeStore = defineStore("theme", () => {
+  const themesList = ref(themeOptions);
+  const activeTheme = ref(themeOptions[0]);
+
+  const changeTheme = (newTheme) => {
+    theme.value = newTheme;
+  };
+
+  return {
+    themesList,
+    activeTheme,
+    changeTheme,
+  };
+});
